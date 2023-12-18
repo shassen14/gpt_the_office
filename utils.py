a21e84a1b017efa6a40d6815c6d3d7d1eed09b0f
Samir Hassen <samirhassen14@gmail.com>
2023-12-17 22:03:34 -0600

    get batch function to obtain batches from dataset

---
+++ utils.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+
+import numpy as np
+import torch
+
+def get_batch(dataset_folder: str,
+              dataset_type: str,
+              batch_size: int,
+              block_size: int,
+              device_type: str) -> tuple[torch.Tensor, torch.Tensor]:
+    # Obtain file path
+    # TODO: change if reorganizing files/directories
+    file_path = os.path.join(os.path.dirname(__file__),
+                             "data/" + dataset_folder + "/" + dataset_type)
+    
+    # save data
+    data = np.memmap(file_path, dtype=np.uint16, mode='r')
+
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+
+    if device_type == 'cuda':
+        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
+        x, y = x.pin_memory().to(device_type, non_blocking=True), y.pin_memory().to(device_type, non_blocking=True)
+    else:
+        x, y = x.to(device_type), y.to(device_type)
+
+    return x, y
