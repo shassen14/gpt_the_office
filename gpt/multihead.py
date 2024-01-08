e0b9b943d4dbfd0304838359312e3ca8efa7c7a9
shassen14 <samirhassen14@gmail.com>
2024-01-07 22:38:49 -0600

    attempt to make classes for self attention

---
+++ gpt/multihead.py
@@ -0,0 +1,24 @@
+import gpt.singlehead as sh
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class Attention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+    def __init__(self, block_size, n_embeddings, head_size, num_heads, dropout):
+        super().__init__()
+        self.heads = nn.ModuleList([sh.Attention(block_size, n_embeddings, head_size, dropout)
+                                    for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embeddings)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+    
+if __name__ == "__main__":
+
+    Attention(10, 10, 10, 10, 0.2)
+    print("hello")
---
+++ gpt/singlehead.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class Attention(nn.Module):
+    """ one head of self-attention """
+    def __init__(self, block_size, n_embeddings, head_size, dropout):
+        super().__init__()
+        self.key = nn.Linear(n_embeddings, head_size, bias=False)
+        self.query = nn.Linear(n_embeddings, head_size, bias=False)
+        self.value = nn.Linear(n_embeddings, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        weight = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        weight = weight.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        weight = F.softmax(weight, dim=-1) # (B, T, T)
+        weight = self.dropout(weight)
+        # perform the weightghted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = weight @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
