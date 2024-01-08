d1c25a3ac80e12f10fc739d31efc0ae8ca2bdf10
shassen14 <samirhassen14@gmail.com>
2024-01-07 22:39:08 -0600

    update to configs

--- config/base.py
+++ config/base.py
@@ -12,8 +12,15 @@ if choose_config == 'cfg1':
     import config.cfg1 as cfg
 
 ############################## Paramaters #####################################
-block_size = cfg.block_size
-batch_size = cfg.batch_size
+block_size      = cfg.block_size
+batch_size      = cfg.batch_size
+n_embeddings    = cfg.n_embeddings
+
+head_size       = cfg.head_size
+num_heads       = cfg.num_heads
+
+dropout         = cfg.dropout
+
 
 # get device type. get GPU or apple if possible
 device_type = 'cpu'
--- config/cfg1.py
+++ config/cfg1.py
@@ -2,3 +2,10 @@
 batch_size = 4
 block_size = 8
 n_embeddings = 32
+
+
+## multihead
+head_size = 5
+num_heads = 5
+
+dropout = 0.2
