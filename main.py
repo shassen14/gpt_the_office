08b0445e02a4deaeb9b36c3ca261d4a387a5d2a3
Samir Hassen <samirhassen14@gmail.com>
2024-01-14 20:40:52 -0600

    change test code

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import utils
 import config.base as cfg
-import gpt.singlehead as sh
-import gpt.multihead as mh
+import gpt.model as gm
 
+import pickle
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+import os
 
 torch.set_printoptions(precision=2, linewidth=100)
 
@@ -17,41 +19,34 @@ channels = 32
 head_size = 16
 
 if __name__ == "__main__":
-    # testing code stuff
-    # input, next = utils.get_batch("shakespeare",
-    #                               "train.bin",
-    #                               cfg.block_size,
-    #                               cfg.batch_size,
-    #                               cfg.device_type)
+    # obtain vocabulary size from pkl
+    pickle_path = utils.get_dataset_path('shakespeare',
+                                         'meta.pkl')
+    if os.path.exists(pickle_path):
+        with open(pickle_path, 'rb') as f:
+            meta = pickle.load(f)
+        meta_vocab_size = meta['vocab_size']
+        print(f"found vocab_size = {meta_vocab_size} (inside {pickle_path})")
+    else:
+        print("pkl file doesn't exist. Please input a valid one.")
+        exit()
 
-    sh.Attention(cfg.block_size, cfg.n_embeddings, cfg.head_size, cfg.dropout)
+    model = gm.GPT(meta_vocab_size,
+                   cfg.num_layers,
+                   cfg.block_size,
+                   cfg.n_embeddings,
+                   cfg.head_size,
+                   cfg.num_heads,
+                   cfg.dropout,
+                   cfg.device_type)
 
-    mh.Attention(cfg.block_size, cfg.n_embeddings, cfg.head_size, cfg.num_heads, cfg.dropout)
 
+    # print(chars)
 
-    # x = torch.randn(batch, time, channels)  # B, T, C
+    input, next = utils.get_batch("shakespeare",
+                                  "train.bin",
+                                  cfg.block_size,
+                                  cfg.batch_size,
+                                  cfg.device_type)
     
-    # # single head
-    # key = nn.Linear(channels, head_size, bias=False)    # C -> H
-    # query = nn.Linear(channels, head_size, bias=False)  # C -> H
-
-    # k = key(x)      # B, T, H
-    # q = query(x)    # B, T, H
-
-    # weights = q @ k.transpose(1, 2) # (B, T, H) @ (B, H, T) = (B, T, T)
-
-    # # lower triangle of (T, T) matrix with 1s
-    # # then replace the upper triangle of 0s in tril to -inf for weights
-    # tril = torch.tril(torch.ones(time, time))
-    # weights = weights.masked_fill(tril == 0, float('-inf'))
-    
-    # # weights = F.softmax(weights, dim=-1)
-    # output = weights @ x    # (B, T, T) @ (B, T, C) = (B, T, C) 
-
-    # print(x.shape)
-    # print(k.shape)
-    # # print(weights[0])
-    # print(output[0])
-
-
-    print("Hello World!")
+    print(input)
