8d71c268894406e241371045be28c30dd017f07b
Samir Hassen <samirhassen14@gmail.com>
2024-01-15 11:18:05 -0600

    added training in main.py

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ import os
 
 torch.set_printoptions(precision=2, linewidth=100)
 
-batch = 4
-time = 8
-channels = 32
-
-head_size = 16
+# @torch.no_grad()
+# def estimate_loss(model,
+#                   ):
+#     out = {}
+#     model.eval()
+#     for split in ['train.bin', 'val.bin']:
+#         losses = torch.zeros(eval_iters)
+#         for k in range(eval_iters):
+#             X, Y = get_batch(split)
+#             logits, loss = model(X, Y)
+#             losses[k] = loss.item()
+#         out[split] = losses.mean()
+#     model.train()
+#     return out
 
 if __name__ == "__main__":
     # obtain vocabulary size from pkl
@@ -31,6 +40,8 @@ if __name__ == "__main__":
         print("pkl file doesn't exist. Please input a valid one.")
         exit()
 
+    # create model using config params
+    # convert model to the device. important if using cuda
     model = gm.GPT(meta_vocab_size,
                    cfg.num_layers,
                    cfg.block_size,
@@ -39,14 +50,26 @@ if __name__ == "__main__":
                    cfg.num_heads,
                    cfg.dropout,
                    cfg.device_type)
+    m = model.to(cfg.device_type)
 
+    # print the number of parameters in the model
+    print(sum(p.numel() for p in m.parameters())/1e6, 'million parameters')
 
-    # print(chars)
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.learning_rate)
 
-    input, next = utils.get_batch("shakespeare",
-                                  "train.bin",
-                                  cfg.block_size,
-                                  cfg.batch_size,
-                                  cfg.device_type)
-    
-    print(input)
+    # Iterate max iteration amount of times 
+    for i in range(cfg.max_iterations):
+        xb, yb = utils.get_batch('shakespeare',
+                                 'train.bin',
+                                 cfg.block_size,
+                                 cfg.batch_size,
+                                 cfg.device_type)
+        
+        logits, loss = m(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+        if i % cfg.eval_iterations == 0 or i == cfg.max_iterations - 1:
+            print(loss)
