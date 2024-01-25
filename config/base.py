2357f587aa81f401491aeacc44f170967c06bc3c
Samir Hassen <samirhassen14@gmail.com>
2024-01-25 00:23:22 -0600

    developed a config dataclass to make function/class decleration have less parameters

--- config/base.py
+++ config/base.py
@@ -16,44 +16,47 @@ elif choose_config == 'test_tiktoken_cfg':
     import config.test_tiktoken_cfg as cfg
 elif choose_config == 'small_cfg':
     import config.small_cfg as cfg
+else:
+    print(f"config, {choose_config}, isn't valid. Please choose or add a valid one.")
+    exit()
 
-# @dataclass
-# class Config:
-# Dataset to utilize
-dataset_dir: str        = cfg.dataset_dir
-train_file: str         = cfg.train_file
-val_file: str           = cfg.val_file
-pkl_file: str           = cfg.pkl_file
-file_array: list        = [cfg.train_file, val_file]
+@dataclass
+class Config:
+    # Dataset to utilize
+    dataset_dir: str        = cfg.dataset_dir
+    train_file: str         = cfg.train_file
+    val_file: str           = cfg.val_file
+    pkl_file: str           = cfg.pkl_file
+    file_array              = [cfg.train_file, val_file]
 
-# Parameter Save/Load
-param_dir: str          = cfg.param_dir
-pt_file: str            = cfg.pt_file
+    # Parameter Save/Load
+    param_dir: str          = cfg.param_dir
+    pt_file: str            = cfg.pt_file
 
-## Generate sample texts
-sample_dir: str         = cfg.sample_dir
-sample_file: str        = cfg.sample_file
-max_new_tokens: int     = cfg.max_new_tokens
+    ## Generate sample texts
+    sample_dir: str         = cfg.sample_dir
+    sample_file: str        = cfg.sample_file
+    max_new_tokens: int     = cfg.max_new_tokens
 
-############################## Paramaters #####################################
-block_size: int         = cfg.block_size
-batch_size: int         = cfg.batch_size
-num_embeddings: int     = cfg.num_embeddings
-num_heads: int          = cfg.num_heads
-num_layers: int         = cfg.num_layers
-head_size: int          = num_embeddings // num_heads
-dropout: float          = cfg.dropout
+    ############################## Paramaters #####################################
+    block_size: int         = cfg.block_size
+    batch_size: int         = cfg.batch_size
+    num_embeddings: int     = cfg.num_embeddings
+    num_heads: int          = cfg.num_heads
+    num_layers: int         = cfg.num_layers
+    head_size: int          = num_embeddings // num_heads
+    dropout: float          = cfg.dropout
 
-learning_rate: float    = cfg.learning_rate
-max_iterations: int     = cfg.max_iterations
-eval_iterations: int    = int(max_iterations / 10)
+    learning_rate: float    = cfg.learning_rate
+    max_iterations: int     = cfg.max_iterations
+    eval_iterations: int    = int(max_iterations / 10)
 
-# get device type. get GPU or apple if possible
-device_type: str        = 'cpu'
+    # get device type. get GPU or apple if possible
+    device_type: str        = 'cpu'
 
-if torch.cuda.is_available():
-    device_type = "cuda"
-elif torch.backends.mps.is_available():
-    device_type = 'mps'
+    if torch.cuda.is_available():
+        device_type = "cuda"
+    elif torch.backends.mps.is_available():
+        device_type = 'mps'
 
-print("Device Type: " + device_type)
+    print("Device Type: " + device_type)
